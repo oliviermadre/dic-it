81027ac304097b4788e25f0d8c40366adc9f28bf
Thibaud Fabre <thibaud@evaneos.com>
2014-02-22 14:00:18 +0100

    Remove fluent interface on registry

--- src/DICIT/Registry.php
+++ src/DICIT/Registry.php
@@ -9,13 +9,19 @@ class Registry
 
     }
 
+    /**
+     * Flush all stored instances from the registry.
+     * @return \DICIT\Registry
+     */
     public function flush() {
         $this->data = array();
         return $this;
     }
 
     /**
+     * Fetches an object from the registry.
      * @param string $key
+     * @return mixed
      */
     public function get($key, $throwIfNotFound = false) {
         if (array_key_exists($key, $this->data)) {
@@ -30,10 +36,11 @@ class Registry
     }
 
     /**
+     * Stores an object instance in the registry.
      * @param string $key
+     * @param mixed $value
      */
     public function set($key, & $value) {
         $this->data[$key] = & $value;
-        return $this;
     }
 }
