2a0de00992303c4a7021d0efcb09c5c7adbdb84f
Thibaud Fabre <thibaud@evaneos.com>
2014-02-27 18:28:38 +0100

    Add test case for multiple method invocations

--- tests/unit/Injectors/MethodInjectorTest.php
+++ tests/unit/Injectors/MethodInjectorTest.php
@@ -1,10 +1,11 @@
 <?php
-
 namespace DICIT\Tests\Injectors;
 
 use DICIT\Injectors\MethodInjector;
+
 class MethodInjectorTest extends \PHPUnit_Framework_TestCase
 {
+
     public function testDefinedMethodsAreInvoked()
     {
         $mock = $this->getMock('\DICIT\Tests\Injectors\TestInjectable');
@@ -28,12 +29,36 @@ class MethodInjectorTest extends \PHPUnit_Framework_TestCase
 
         $actual = $injector->inject($container, $mock, $serviceConfig);
     }
-}
 
-class TestInjectable {
+    public function testArrayOfParameterArraysInvokesInjectionMultipleTimes()
+    {
+        $mock = $this->getMock('\DICIT\Tests\Injectors\TestInjectable');
 
-    public function setMethod($value) {
+        $mock->expects($this->exactly(2))
+            ->method('setMethod')
+            ->with($this->equalTo(2));
 
+        $container = $this->getMockBuilder('\DICIT\Container')
+            ->disableOriginalConstructor()
+            ->setMethods(array('resolveMany'))
+            ->getMock();
+
+        $container->expects($this->once())
+            ->method('resolveMany')
+            ->will($this->returnValue(array(2)));
+
+        $injector = new MethodInjector();
+
+        $serviceConfig = array('call' => array('setMethod[0]' => array('value'), 'setMethod[1]' => array('value2')));
+
+        $actual = $injector->inject($container, $mock, $serviceConfig);
     }
 
+}
+
+class TestInjectable
+{
+
+    public function setMethod($value)
+    {}
 }
