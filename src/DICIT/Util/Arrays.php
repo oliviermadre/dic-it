b7496f9d9115fab555dd965ea6914234586ffa63
remievaneos <remi@evaneos.com>
2014-06-10 17:39:49 +0200

    changed array merge see @oliviermadre

--- src/DICIT/Util/Arrays.php
+++ src/DICIT/Util/Arrays.php
@@ -4,49 +4,68 @@ namespace DICIT\Util;
 class Arrays
 {
 
-    public static function mergeRecursiveUnique($array0, $array1) {
-        $arrays = func_get_args();
-        $remains = $arrays;
-
-        // We walk through each arrays and put value in the results (without
-        // considering previous value).
-        $result = array();
-
-        // loop available array
-        foreach($arrays as $array) {
-
-            // The first remaining array is $array. We are processing it. So
-            // we remove it from remaing arrays.
-            array_shift($remains);
-
-            // We don't care non array param, like array_merge since PHP 5.0.
-            if(is_array($array)) {
-                // Loop values
-                foreach($array as $key => $value) {
-                    if(is_array($value)) {
-                        // we gather all remaining arrays that have such key available
-                        $args = array();
-                        foreach($remains as $remain) {
-                            if(array_key_exists($key, $remain)) {
-                                array_push($args, $remain[$key]);
-                            }
-                        }
-
-                        if(count($args) > 2) {
-                            // put the recursion
-                            $result[$key] = call_user_func_array(__FUNCTION__, $args);
-                        } else {
-                            foreach($value as $vkey => $vval) {
-                                $result[$key][$vkey] = $vval;
-                            }
-                        }
-                    } else {
-                        // simply put the value
-                        $result[$key] = $value;
-                    }
-                }
+    public static function mergeRecursiveUnique ( array $array1, array $array2 )
+    {
+        $merged = $array1;
+    
+        foreach ( $array2 as $key => $value )
+        {
+            if ( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) )
+            {
+                $merged [$key] = self::mergeRecursiveUnique ( $merged [$key], $value );
+            }
+            else
+            {
+                $merged [$key] = $value;
             }
         }
-        return $result;
+    
+        return $merged;
     }
+    
+//     public static function mergeRecursiveUnique($array0, $array1) {
+//         $arrays = func_get_args();
+//         $remains = $arrays;
+
+//         // We walk through each arrays and put value in the results (without
+//         // considering previous value).
+//         $result = array();
+
+//         // loop available array
+//         foreach($arrays as $array) {
+
+//             // The first remaining array is $array. We are processing it. So
+//             // we remove it from remaing arrays.
+//             array_shift($remains);
+
+//             // We don't care non array param, like array_merge since PHP 5.0.
+//             if(is_array($array)) {
+//                 // Loop values
+//                 foreach($array as $key => $value) {
+//                     if(is_array($value)) {
+//                         // we gather all remaining arrays that have such key available
+//                         $args = array();
+//                         foreach($remains as $remain) {
+//                             if(array_key_exists($key, $remain)) {
+//                                 array_push($args, $remain[$key]);
+//                             }
+//                         }
+
+//                         if(count($args) > 2) {
+//                             // put the recursion
+//                             $result[$key] = call_user_func_array(__FUNCTION__, $args);
+//                         } else {
+//                             foreach($value as $vkey => $vval) {
+//                                 $result[$key][$vkey] = $vval;
+//                             }
+//                         }
+//                     } else {
+//                         // simply put the value
+//                         $result[$key] = $value;
+//                     }
+//                 }
+//             }
+//         }
+//         return $result;
+//     }
 }
