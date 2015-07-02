875fe2b5067658da2f7f1b0d649c54dcafa5aaa8
Olivier Madre <terenas@gmail.com>
2015-07-02 20:13:37 +0200

    can inject a proxy cache configuration

--- src/DICIT/ActivatorFactoryLazy.php
+++ src/DICIT/ActivatorFactoryLazy.php
@@ -9,13 +9,15 @@ use DICIT\Activators\LazyActivator;
 use DICIT\Activators\RemoteActivator;
 use DICIT\Activators\RemoteAdapterFactory;
 
+use ProxyManager\Configuration;
+
 class ActivatorFactoryLazy extends ActivatorFactorySimple
 {
-    public function __construct()
+    public function __construct(Configuration $configuration = null)
     {
-        $this->addActivator('builder-static', new LazyActivator(new StaticInvocationActivator()), 1);
-        $this->addActivator('builder', new LazyActivator(new InstanceInvocationActivator()), 1);
-        $this->addActivator('remote', new LazyActivator(new RemoteActivator(new RemoteAdapterFactory())), 1);
-        $this->addActivator('default', new LazyActivator(new DefaultActivator()), 100);
+        $this->addActivator('builder-static', new LazyActivator(new StaticInvocationActivator(), $configuration), 1);
+        $this->addActivator('builder', new LazyActivator(new InstanceInvocationActivator(), $configuration), 1);
+        $this->addActivator('remote', new LazyActivator(new RemoteActivator(new RemoteAdapterFactory()), $configuration), 1);
+        $this->addActivator('default', new LazyActivator(new DefaultActivator(), $configuration), 100);
     }
 }
